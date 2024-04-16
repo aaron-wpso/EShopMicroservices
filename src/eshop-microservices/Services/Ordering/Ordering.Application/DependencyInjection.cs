bc2292275cfa5af4240d07480156ffd7477c4c66
Aaron <aaron.weipingso98@gmail.com>
2024-04-17 00:00:17 +0800

    Finished building Ordering.Application

--- src/eshop-microservices/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/eshop-microservices/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BuildingBlocks.Behaviors;
 
 namespace Ordering.Application;
 
@@ -6,9 +6,12 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        //services.AddMediatR(cfg => {
-        //      cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-        //});
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
 
         return services;
     }
--- src/eshop-microservices/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/eshop-microservices/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace Ordering.Infrastructure;
+﻿namespace Ordering.Infrastructure;
 
 public static class DependencyInjection
 {
@@ -11,9 +8,17 @@ public static class DependencyInjection
         var connectionString = configuration.GetConnectionString("Database");
 
         //Add services to the container.
-        //services.AddDbContext<ApplicationDbContext>(options => 
-        //      options.UseSqlServer(connectionString));
-        //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+        services.AddDbContext<ApplicationDbContext>((sp, options) =>
+
+        {
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+            options.UseSqlServer(connectionString);
+        });
+        
+        services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 
         return services;
     }
